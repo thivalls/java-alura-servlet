66350813bcfc8272330598b31a6d506c8be4c76c
thivalls <olavalls@gmail.com>
2021-02-03 11:28:21 -0300

    First step

---
+++ src/br/com/alura/gerenciador/servlet/HelloWorld.java
@@ -0,0 +1,24 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/")
+public class HelloWorld extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello world from servlet - Estou aprendendo finalmente</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+	
+}
